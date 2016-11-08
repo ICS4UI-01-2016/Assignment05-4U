3e4fda49fd3b426ff1a2f95ca64da6f6397f8f0b
hadik9595 <hadik9595@HRH-IC0027320.wrdsb.ca>
2016-11-08 14:11:58 -0500

    Main class done

---
+++ src/hhssadventure/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+/**
+ *
+ * @author hadik9595
+ */
+public class Controller {
+    
+}
---
+++ src/hhssadventure/Map.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hadik9595
+ */
+public class Map {
+     private ArrayList<Screen> screens;
+}
