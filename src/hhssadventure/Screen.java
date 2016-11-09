7142a06ad25f8d65f0ebd64c4380b163a8667590
halll7908 <halll7908@HRH-IC0027341.wrdsb.ca>
2016-11-09 12:55:32 -0500

    Work on the Screen class.

--- src/hhssadventure/Screen.java
+++ src/hhssadventure/Screen.java
@@ -4,10 +4,38 @@
  */
 package hhssadventure;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Scanner;
+
 /**
  *
  * @author halll7908
  */
 public class Screen {
-    
+
+    private int row;
+    private int col;
+    private BufferedImage image;
+    private boolean north;
+    private boolean east;
+    private boolean south;
+    private boolean west;
+
+    public Screen(Scanner input) {
+        // Scan in the image name
+        String imageName = input.next();
+        // find x and y out of the file name
+        
+        
+        try {
+            File file = new File("file.txt");
+            Scanner s = new Scanner(file);
+            while (s.hasNext()) {
+                System.out.println(s.nextInt());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
