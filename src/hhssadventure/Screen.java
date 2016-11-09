549b246a1089e155f193d2f5ab68c2799477b928
tatad6701 <tatad6701@HRH-IC0027335.wrdsb.ca>
2016-11-09 13:21:40 -0500

    Added more methods to the screen (Location and direction)

--- src/hhssadventure/Screen.java
+++ src/hhssadventure/Screen.java
@@ -5,7 +5,9 @@
 package hhssadventure;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Scanner;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -26,6 +28,82 @@ public class Screen {
     public Screen(Scanner input) {
         // Scan in the image name
         String imageName = input.next();
+        // Split the string to grab row and column 
+        String[] split = imageName.split(" ");
+        // Save the row - gets rid of ... 
 
+        // Save the col = gets rid of ...
+
+
+
+        // Load the image file 
+        try {
+            image = ImageIO.read(new File("images/" + imageName + ".txt"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Getting the possible directions
+        north = input.nextBoolean();
+        east = input.nextBoolean();
+        south = input.nextBoolean();
+        west = input.nextBoolean();
+
+        // Move to the next line
+        input.nextLine();
+    }
+
+    public location(Scanner) {
+        // 4 screens (that the system will know
+        // the name of the location (Caf1 etc)
+        // ALSO ADD A METHOD FOR THE NAMES (THE IMAGE DIRECTION, AND THE IMAGE ITSELF
+    }
+
+    public screen() {
+    }
+
+    /**
+     * Depicts whether the user can go north or not
+     *
+     * @return *******************************************
+     */
+    public boolean canGoNorth() {
+        return north;
+    }
+
+    /**
+     * Depicts whether the user can go east or not
+     *
+     * @return ********************************************
+     */
+    public boolean canGoEast() {
+        return east;
+    }
+
+    /**
+     * Depicts whether the user can go south or not
+     *
+     * @return ********************************************
+     */
+    public boolean canGoSouth() {
+        return south;
+    }
+
+    /**
+     * Depicts whether the user can go west or not
+     *
+     * @return ********************************************
+     */
+    public boolean canGoWest() {
+        return west;
+    }
+
+    /**
+     * Load in the image of the adventure
+     *
+     * @return the image needed to be used
+     */
+    public BufferedImage getImage() {
+        return image;
     }
 }
