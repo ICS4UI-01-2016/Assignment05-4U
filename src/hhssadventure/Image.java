f1b6a1d7cab9c40fd7535cc0fbe7e569dc522795
pircn0556 <pircn0556@HRH-IC0027340.wrdsb.ca>
2016-11-09 12:55:22 -0500

    Done image file

--- src/hhssadventure/Image.java
+++ src/hhssadventure/Image.java
@@ -4,10 +4,32 @@
  */
 package hhssadventure;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.swing.JComponent;
+
 /**
  *
  * @author halll7908
  */
-public class Image {
+public class Image extends JComponent {
+
+    private BufferedImage image;
+
+    public Image() {
+        image = null;
+    }
+
+    public void paintComponent(Graphics g) {
+        if (image != null) {
+            g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+        }
+    }
     
+    public void setImage(BufferedImage img){
+        image = img;
+        
+        //so we can see the change being made
+        repaint();
+    }
 }
