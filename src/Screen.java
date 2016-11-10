99c98ce52692d58cb840e7ceed64eecc9436d1e6
Peter <Peter@192.168.0.12>
2016-11-10 14:07:49 -0500

    Done

--- src/Screen.java
+++ src/Screen.java
@@ -1,6 +1,8 @@
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Scanner;
+import javax.imageio.ImageIO;
 
 /*
  * To change this template, choose Tools | Templates
@@ -12,7 +14,6 @@ import java.util.Scanner;
  */
 public class Screen {
     // instance variables
-
     private Screen northConnect;
     private Screen eastConnect;
     private Screen southConnect;
@@ -21,22 +22,82 @@ public class Screen {
     private BufferedImage eastImage;
     private BufferedImage southImage;
     private BufferedImage westImage;
+    private String northImageName;
+    private String eastImageName;
+    private String southImageName;
+    private String westImageName;
     private String north;
     private String east;
     private String south;
     private String west;
-    private Boolean forwards;
+    private Boolean forward;
+    private String name;
 
     public Screen(Scanner input) {
-        // scan in the direction the photo is facing 
-        north = input.next();
-        // scan in the image name
-        String imageName = input.next();
-        // scan in whether you can move forwards
-        forwards = input.nextBoolean();
-        
+        // scan in the name of the location
+        name = input.next();
         // move to next line
         input.nextLine();
+        
+        // scan in the direction the photo is facing 
+        north = input.next();
+        // scan in the north image name
+        northImageName = input.next();
+        // load the north image file
+        try {
+            northImage = ImageIO.read(new File("images/" + northImageName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // scan in whether you can move forwards
+        forward = input.nextBoolean();
+        // move to next line
+        input.nextLine(); 
+
+        // scan in the direction the photo is facing 
+        east = input.next();
+        // scan in the east image name
+        eastImageName = input.next();
+        // load the east image file
+        try {
+            eastImage = ImageIO.read(new File("images/" + eastImageName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // scan in whether you can move forwards
+        forward = input.nextBoolean();
+        // move to next line
+        input.nextLine(); 
+
+        // scan in the direction the photo is facing 
+        south = input.next();
+        // scan in the south image name
+        southImageName = input.next();
+        // load the east image file
+        try {
+            southImage = ImageIO.read(new File("images/" + southImageName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // scan in whether you can move forwards
+        forward = input.nextBoolean();
+        // move to next line
+        input.nextLine(); 
+
+        // scan in the direction the photo is facing 
+        west = input.next();
+        // scan in the west image name
+        westImageName = input.next();
+        // load the east image file
+        try {
+            westImage = ImageIO.read(new File("images/" + westImageName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // scan in whether you can move forwards
+        forward = input.nextBoolean();
+        // move to next line
+        input.nextLine(); 
 
         
         // set all to null - so no error codes for now
@@ -47,6 +108,22 @@ public class Screen {
         
     }
 
+    /**
+     * Method to get whether you can move forward
+     * @return true if you can move forward, false if you 
+     */
+    public boolean canGoForward(){
+        return this.forward;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getName(){
+        return this.name;
+    }
+    
     /**
      * Connect to the north screen
      *
