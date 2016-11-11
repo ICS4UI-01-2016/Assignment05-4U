ce038af4cb6dc96d1e2226247493627bd6286d9c
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-11-11 13:11:07 -0500

    added comments to Controller

--- src/hhssadventure/Controller.java
+++ src/hhssadventure/Controller.java
@@ -20,40 +20,54 @@ public class Controller {
         this.location = location;
         this.direction = direction;
         
+        // make sure the GUI can talk to you
         gui.setController(this);
+        // set the starting image
         Screen start = map.getScreen(location, direction);
         gui.setImage(start.getImage());
     }
     
     public void goNorth(){
+        // get the screen we are on right now
         Screen current = map.getScreen(location, direction);
-        
+        // ask if we can move north
         if(current.canGoNorth()){
+            // get the new screen
             Screen newScreen = map.getScreen(location, direction);
+            // set the image
             gui.setImage(newScreen.getImage());
         }
     }
     
     public void goSouth(){
+        // get the screen we are on right now
         Screen current = map.getScreen(location, direction);
+        // ask if we can move south
         if(current.canGoSouth()){
+            // get the new screen
             Screen newScreen = map.getScreen(location, direction);
+            // set the image
             gui.setImage(newScreen.getImage());
         }
     }
     
     public void goEast(){
+        // get the screen we are on right now
         Screen current = map.getScreen(location, direction);
+        // ask if we can move east
         if(current.canGoEast()){
             Screen newScreen = map.getScreen(location, direction);
             gui.setImage(newScreen.getImage());
         }
     }
-    
     public void goWest(){
+        // get the screen we are on right now
         Screen current = map.getScreen(location, direction);
+        // ask if we can move west
         if(current.canGoWest()){
+            // get the new screen
             Screen newScreen = map.getScreen(location, direction);
+            // set the image
             gui.setImage(newScreen.getImage());
         }
     }
