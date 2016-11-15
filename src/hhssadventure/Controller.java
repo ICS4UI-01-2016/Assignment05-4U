c0f1415613b10bd58bf2982dd35fa894e556a167
tatad6701 <tatad6701@HRH-IC0027335.wrdsb.ca>
2016-11-15 12:47:38 -0500

    Fixed Controller

--- src/hhssadventure/Controller.java
+++ src/hhssadventure/Controller.java
@@ -9,62 +9,65 @@ package hhssadventure;
  * @author richj0985
  */
 public class Controller {
+
     private GUI gui;
     private Map map;
     String location;
     String direction;
-    
-    public Controller(GUI gui, Map map){
+
+    public Controller(GUI gui, Map map) {
         this.gui = gui;
         this.map = map;
-        this.location  = map.getInitialLocation();
+        this.location = map.getInitialLocation();
         this.direction = map.getInitialDirection();
-        
+
         // make sure the GUI can talk to you
         gui.setController(this);
         // set the starting image
         Screen start = map.getScreen(location, direction);
         gui.setImage(start.getImage());
     }
-    
-    public void goNorth(){
+
+    public void goNorth() {
         // get the screen we are on right now
         Screen current = map.getScreen(location, direction);
         // ask if we can move north
-        if(current.canGoNorth()){
+        if (current.isBlocked == false) {
+        
             // get the new screen
             Screen newScreen = map.getScreen(location, direction);
             // set the image
             gui.setImage(newScreen.getImage());
         }
     }
-    
-    public void goSouth(){
+
+    public void goSouth() {
         // get the screen we are on right now
         Screen current = map.getScreen(location, direction);
         // ask if we can move south
-        if(current.canGoSouth()){
+        if (current.isBlocked == false) {
             // get the new screen
             Screen newScreen = map.getScreen(location, direction);
             // set the image
             gui.setImage(newScreen.getImage());
         }
     }
-    
-    public void goEast(){
+
+    public void goEast() {
         // get the screen we are on right now
         Screen current = map.getScreen(location, direction);
         // ask if we can move east
-        if(current.canGoEast()){
+        if (current.isBlocked == false) {
             Screen newScreen = map.getScreen(location, direction);
             gui.setImage(newScreen.getImage());
         }
     }
-    public void goWest(){
+
+    public void goWest() {
         // get the screen we are on right now
         Screen current = map.getScreen(location, direction);
         // ask if we can move west
-        if(current.canGoWest()){
+        if (current.isBlocked == false) {
             // get the new screen
             Screen newScreen = map.getScreen(location, direction);
             // set the image
