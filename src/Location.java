3481263fe826a0fd0971f13abbe932d7a7e09b02
yuk4142 <yuk4142@HRH-IC0027301.wrdsb.ca>
2016-11-21 10:55:56 -0500

    we gave up, switching to location class instead

---
+++ src/Location.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yuk4142
+ */
+public class Location {
+    private String location;
+    
+}
