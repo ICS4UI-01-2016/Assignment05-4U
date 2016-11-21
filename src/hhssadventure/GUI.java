c13068e7910cf89f449837c852da3148c8857a6c
Peter <Peter@Katrina>
2016-11-21 12:30:04 -0500

    setMap added to GUI

--- src/hhssadventure/GUI.java
+++ src/hhssadventure/GUI.java
@@ -25,12 +25,6 @@ public class GUI extends javax.swing.JFrame {
         initComponents();
     }
     
-    public GUI(){
-        initComponents();
-    }
-    
-    
-
     public void setMap(Map m){
         this.map = m;
     }
