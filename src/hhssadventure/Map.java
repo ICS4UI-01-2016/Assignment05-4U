5cf4427fa19eb8e7f9b154a9bdbe3d4d1d8eff2e
brayden <brayden@192.168.0.17>
2016-12-01 12:58:30 -0500

    memes done

--- src/hhssadventure/Map.java
+++ src/hhssadventure/Map.java
@@ -16,56 +16,80 @@ import java.util.Scanner;
  */
 public class Map {
 
+    //instance variables
     private ArrayList<Screen> screens;
     private ArrayList<Area> areas;
     private String startArea;
     private String startDirection;
 
+    /**
+     * constructor class for map
+     *
+     * @param fileName the file being scanned
+     */
     public Map(String fileName) {
-
+        //intialize areas arraylist
         areas = new ArrayList<>();
-
+        //if file exists
         try {
             // make the scanner for the text file
             Scanner in = new Scanner(new File(fileName));
-
+            //scan in start area and direction
             this.startArea = in.next();
             this.startDirection = in.next();
-            
 
-            // Make screens
-            // loop to the end of the file
+            //while there is unscanned text in a line
             while (in.hasNext()) {
+                //create a new area and add it to the array list
                 Area a = new Area(in.next(), in);
                 areas.add(a);
             }
-
+            //catch the printstacktrace exception
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
 
     }
 
+    /**
+     * accessor method to get start area
+     *
+     * @return start area
+     */
     public String getStartArea() {
         return this.startArea;
     }
 
+    /**
+     * accessor method to get start direction
+     *
+     * @return start direction
+     */
     public String getStartDirection() {
         return this.startDirection;
     }
 
+    /**
+     * accessor method to get screen
+     *
+     * @param Area area of the screen
+     * @param Direction direction of the screen
+     * @return the area and the direction
+     */
     public Screen getScreen(String Area, String Direction) {
         // go through each screen
         for (Area a : areas) {
+            //pass in screens for area a
             this.screens = a.getScreens();
+            //for each screen check to see if the area name and direction are equal
             for (Screen s : screens) {
                 if (s.getAreaName().equalsIgnoreCase(Area) && s.getDirection().equalsIgnoreCase(Direction)) {
-                    // I found the screen
+                    //return s if found the screen
                     return s;
                 }
             }
         }
-        // no screen found
+        //return null if no screen found
         return null;
     }
 
--- src/hhssadventure/Screen.java
+++ src/hhssadventure/Screen.java
@@ -16,6 +16,7 @@ import javax.imageio.ImageIO;
  */
 public class Screen {
 
+    //instance variables
     private String areaName;
     private String direction;
     private BufferedImage image;
@@ -23,54 +24,90 @@ public class Screen {
     private String aftrArea;
     private String aftrDir;
 
+    /**
+     * Constructor of the screen
+     *
+     * @param AreaName string containing the area name
+     * @param in the scanner
+     */
     public Screen(String AreaName, Scanner in) {
-
+        //pass in area name and direction of intial area
         this.areaName = AreaName;
-
         this.direction = in.next();
-
+        //scan in image name
         String imageName = in.next();
-
+        //catch printStackTrace exception
         try {
             this.image = ImageIO.read(new File("images/" + imageName));
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        //if front is not clear set canMove to false
         if (in.next().equals("true")) {
             this.canMove = false;
+            //if front is clear set canMove to true
         } else {
             this.canMove = true;
         }
-
+        //can canMove is true scan in the following area and direction
         if (this.canMove) {
             this.aftrArea = in.next();
             this.aftrDir = in.next();
         }
     }
 
+    /**
+     * accessor method to get current area
+     *
+     * @return current area
+     */
     public String getAreaName() {
         return this.areaName;
     }
 
+    /**
+     * accessor method to get current direction
+     *
+     * @return current direction
+     */
     public String getDirection() {
         return this.direction;
     }
 
+    /**
+     * accessor method to get current image
+     *
+     * @return current image
+     */
     public BufferedImage getImage() {
         return this.image;
     }
 
+    /**
+     * accessor method to get whether front is clear
+     *
+     * @return can move boolean
+     */
     public boolean getCanMove() {
         return this.canMove;
     }
 
+    /**
+     * accessor method to get next area
+     *
+     * @return next area
+     */
     public String getAftrArea() {
         return this.aftrArea;
     }
 
+    /**
+     * accessor method to get next direction
+     *
+     * @return next direction
+     */
     public String getAftrDir() {
         return this.aftrDir;
     }
-    
+
 }
