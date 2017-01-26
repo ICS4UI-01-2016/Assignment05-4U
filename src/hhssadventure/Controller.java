37a4f894d7ee134a0132de2f16556f7aa44cfcb0
Denise <Denise@192.168.0.17>
2017-01-26 16:45:30 -0500

    restarting the game cause the old one is a mess

---
+++ src/hhssadventure/Controller.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+/**
+ *
+ * @author Denise
+ */
+public class Controller {
+    
+}
--- src/hhssadventure/HHSSAdventure.java
+++ src/hhssadventure/HHSSAdventure.java
@@ -15,7 +15,13 @@ public class HHSSAdventure {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        GUI gui = new GUI();
+        
+        
+        
+        
+        
     }
     
 }
