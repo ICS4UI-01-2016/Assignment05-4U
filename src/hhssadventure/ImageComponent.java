7bc0ed562ab9d590d9667014571e89d70eec759e
Denise <Denise@192.168.0.17>
2017-01-27 20:30:50 -0500

    commenting code

--- src/hhssadventure/ImageComponent.java
+++ src/hhssadventure/ImageComponent.java
@@ -14,19 +14,29 @@ import javax.swing.JComponent;
  * @author Brendan
  */
 public class ImageComponent extends JComponent {
-
+    // creating a variable for the image
     private BufferedImage image;
-
+    /**
+     * constructing the image
+     */
     public ImageComponent() {
         image = null;
     }
-
+    /**
+     * a method for drawing the image
+     * @param g variable to represent the graphics
+     */
     public void drawComponent(Graphics g) {
-        if (image != null) {
+        // if there is an image to draw do the following 
+        if (image != null) { // 
             g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
         }
     }
 
+    /**
+     * draw the image on the screen 
+     * @param img variable to represent the image
+     */
     public void drawImage(BufferedImage img) {
         image = img;
         repaint();
--- src/hhssadventure/Location.java
+++ src/hhssadventure/Location.java
@@ -14,19 +14,32 @@ import java.util.Scanner;
  */
 public class Location {
 
+    // creating an array list to store all of the screens for a given area
     private ArrayList<Screen> screens;
 
+    /**
+     * constructing an area that will have smaller areas within it
+     * @param locationName the name of the location in the picture file
+     * @param in the variable used for the scanner
+     */
     public Location(String locationName, Scanner in) {
+        // creating the array list containing the screens for an area
         screens = new ArrayList<>();
 
+        // make three screens for an area and do the following 
         for (int i = 0; i <= 3; i++) {
+            // construct a new screen with the locations name and the scanner
             Screen s = new Screen(locationName, in);
+            // add the new screen to the array list
             screens.add(s);
         }
     }
 
+    /**
+     * return the array list with all the screens
+     * @return the screens array list
+     */
     public ArrayList getNextScreen() {
         return screens;
     }
-
 }
